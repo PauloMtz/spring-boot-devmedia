50a0ce4c5d23cb32a6ebbe1435eab38016f78198
PauloMtz <pramosmtz@gmail.com>
2019-06-20 17:58:35 -0300

    Atualizando playlist

--- src/main/java/com/devmedia/playlist/controller/PlaylistController.java
+++ src/main/java/com/devmedia/playlist/controller/PlaylistController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -42,6 +44,24 @@ public class PlaylistController {
 		
 		playlistService.salvar(playlist);
 		attr.addFlashAttribute("mensagem", "Playlist criada com sucesso.");
+		return "redirect:/playlists/listar";
+	}
+	
+	@GetMapping("/{id}/atualizar")
+	public ModelAndView preAtualizar(@PathVariable("id") long id, ModelMap model) {
+		Playlist playlist = playlistService.buscarPorId(id);
+		model.addAttribute("playlist", playlist);
+		return new ModelAndView("/playlist/adicionar", model);
+	}
+	
+	@PutMapping("/salvar")
+	public String atualizar(@Valid @ModelAttribute("playlist") Playlist playlist, BindingResult result, RedirectAttributes attr) {
+		if(result.hasErrors()) {
+			return "/playlist/adicionar";
+		}
+		
+		playlistService.atualizar(playlist);
+		attr.addFlashAttribute("mensagem", "Playlist atualizada com sucesso.");
 		return "redirect:/playlists/listar";
 	}
 }
